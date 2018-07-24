9d7c91a763b31d554506275d06f5751f2518a6b1
xujia <1422941648@qq.com>
2018-07-24 17:06:05 +0800

    hello

--- 7.24/hello.py
+++ 7.24/hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello ChouGui!'
+    return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run()
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5000,debug=True)
