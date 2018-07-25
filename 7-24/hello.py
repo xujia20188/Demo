bcc15b3fd21f99bcdf9811b30013ff8abcb140b0
xujia <1422941648@qq.com>
2018-07-25 15:58:30 +0800

    15_00

--- 7-24/hello.py
+++ 7-24/hello.py
@@ -9,3 +9,9 @@ def hello_world():
 @app.route('/hello')
 def hello():
     return 'Hello, World'
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=5000,debug=True)
---
+++ 7-25/fisher.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+__author__='七月'
+
+app = Flask(__name__)
+
+app.run()
