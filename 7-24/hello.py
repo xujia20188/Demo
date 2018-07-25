c687922e8727e85e9de711af18db16ece19ba597
xujia <1422941648@qq.com>
2018-07-25 10:36:08 +0800

    10-00

--- 7-24/hello.py
+++ 7-24/hello.py
@@ -6,10 +6,6 @@ def hello_world():
     return 'Hello, World!'
 
 
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=5000,debug=True)
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
