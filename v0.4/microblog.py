b46a7548b96016b815789c6d9d17976b30cf3c0e
xujia <1422941648@qq.com>
2018-08-13 09:24:29 +0800

    commit

---
+++ v0.4/microblog.py
@@ -0,0 +1,12 @@
+from app import app, db
+from app.models import User, Post
+
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1",port=8000,debug=True)
